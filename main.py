c94f6f0c026eb032febb4ad5b67e10b6206f5ff5
Michael Abbott <michael.abbott@rochester.edu>
2024-04-09 15:13:35 -0400

    added docstrings

--- main.py
+++ main.py
@@ -10,7 +10,19 @@ df = pd.DataFrame({
 })
 
 def evaluate_data(df:pd.DataFrame, comparison_groups:list[list]):
+    '''Variable summaries with tests for consistency across groups.
     
+    Arguments:
+        df:
+            A dataframe to be analyzed.
+        comparison_groups:
+            A list of lists of variables that defined groups across which
+            we want to compare variables.
+    
+    Returns:
+        info_dict:
+            A dictionary containing all the relevant information
+    '''
     # Define a dictionary where all info will go
     info_dict = dict()
     # Define a dict to store all of the things you may want to look at
@@ -69,9 +81,38 @@ def do_comparison(
     type:str,
     var_stats:dict
 ):
+    '''Compare a variable's values across subsets of the sample.
+    
+    Arguments:
+        comparison_group:
+            A list of variable names that define the distinct groups to
+            be compared. E.G. if it's ['a','b'], then each unique
+            combination of values for 'a' and 'b' will be treated as
+            a group.
+        var:
+            The name of the variable to be analyzed.
+        has_nulls:
+            A bool indicating if var has null values in the sample.
+        var_df:
+            A dataframe including var and the comparison group variables
+        type:
+            A string describing the "type" of var. This is NOT the python
+            -type. It will be from the following list defined of options:
+                [Constant, Constant With Nulls, Binary Bool,
+                Binary Numerical, Binary Categorical, Numerical,
+                Categorical]   
+            See function "get_variable_type()" for more info.
+        var_stats:
+            A dictionary of summary stats about the variable.
+
+    Returns:
+        comparison_dict:
+            A dictionary with comparison results for var, across
+            comparison_group.
+    '''
     # Instantiate Comparison
     comparison_dict = dict()
-    
+
     # Compare nulls
     comparison_dict['Nulls'] = compare_nulls_across_groups(
         comparison_group=comparison_group,
@@ -79,7 +120,7 @@ def do_comparison(
         has_nulls = has_nulls,
         var_df = var_df
     )
-    
+
     # Now we do some other comparisons based on the variable type
     if type in ['Constant','Constant With Nulls']:
         pass
@@ -99,8 +140,16 @@ def do_comparison(
         # TODO comparison_dict['Mean'] = compare_mean()
         
     elif type in ['Categorical', 'Binary Categorical']:
-        comparison_dict['Value Shares'] = compare_all_value_shares()
-        comparison_dict['Unique Values'] = compare_unique_values()
+        comparison_dict['Value Shares'] = compare_all_value_shares(
+            var_df=var_df,
+            comparison_group=comparison_group,
+            var=var
+        )
+        comparison_dict['Unique Values'] = compare_unique_values(
+            var_df=var_df,
+            comparison_group=comparison_group,
+            var=var
+        )
         comparison_dict['Still Need to edit this'] = 'really'
     
     return comparison_dict
@@ -110,7 +159,27 @@ def compare_unique_values(
     comparison_group:list[str],
     var:str,
 ):
-    unique_vals_dict = dict()
+    '''Find inconsistencies in the values exhibited by categorical var.
+    
+    Arguments:
+        var_df:
+            A dataframe with var and the comparison_group variables.
+        comparison_group:
+            The group of variables whose unique combination of values
+            defines a group.
+        var:
+            The name of the variable to be analyzed.
+    
+    Returns:
+        string 'All Groups Share Common Support'
+            It will return this string if var has the same support for
+            all groups in comparison_group.
+        vals_of_interest_df:
+            A dataframe which contains the values of var which show up in
+            some but not all groups. for each of these values, you can
+            see the list of groups in which it does show up and the list
+            of groups in which it does not show up.
+    '''
     
     unique_ser = (
         var_df
@@ -125,7 +194,6 @@ def compare_unique_values(
     if len(vals_of_interest) <= 0:
         return 'All Groups Share Common Support'
     else:
-        val_of_interest_df = pd.DataFrame()
         groups_including_col = []
         groups_excluding_col = []
         for val in vals_of_interest:
@@ -138,18 +206,34 @@ def compare_unique_values(
                     groups_without_val.append(list(row[comparison_group]))
             groups_including_col.append(groups_with_val)
             groups_excluding_col.append(groups_without_val)
-        val_of_interest_df = pd.DataFrame({
+        vals_of_interest_df = pd.DataFrame({
             'Value': vals_of_interest,
             'Groups Including Value': groups_including_col,
             'Groups Excluding Value': groups_excluding_col
         }).set_index('Value')
-    return val_of_interest_df
+    return vals_of_interest_df
 
 def compare_all_value_shares(
     var_df:pd.DataFrame,
     var:str,
     comparison_group:list[str]
 ):
+    '''Do a proportion test for each val in a categorical variable.
+    
+    Arguments:
+        var_df:
+            A dataframe containing var and the comparison group variables
+        var:
+            The name of the variable to be analyzed.
+        comparison_group:
+            The list of variables for which a unique combination of
+            values defines a group.
+    Returns:
+        value_shares_dict:
+            a dictionary where the dictionary-keys are values of var, and
+            the dictionary-values contain test results for if the share
+            of the sample with that value is the same across groups.
+    '''
     value_shares_dict = dict()
     for val in var_df[var].unique():
         value_shares_dict[val] = compare_value_share(
@@ -167,6 +251,29 @@ def compare_all_frequency_outliers(
     var:str,
     comparison_group:list[str]
 ):
+    '''For unusually frequent vals of a numerical var, is the proportion
+    of the sample taking that val consistent across groups.
+    
+    Arguments:
+        var_stats:
+            The var_stats dictionary containing information on whether
+            there are actually outliers.
+        var_df:
+            A dataframe containing the values of the var being analyzed
+            and the variables in the comparison_group.
+        var:
+            The name of the analysis variable.
+        comparison_group:
+            The variables whose unique combination of values defines a 
+            group.
+    Returns:
+        string 'No Frequency Outliers'
+            This string is returned if there are no frequency outliers.
+        freq_outliers_dict:
+            A dictionary with info for each value about the test of
+            whether the share of the sample taking that value is
+            consistent across groups.
+    '''
     if var_stats['Frequency Outlier Count'] <= 0:
         return 'No Frequency Outliers'
     else:
@@ -187,6 +294,25 @@ def compare_value_share(
     var:str,
     comparison_group:list[str]
 ):
+    '''Test if the share of sample with a value is cross-group consistent.
+    
+    Arguments:
+        value:
+            The value whose frequency is being tested.
+        var_df:
+            A dataframe containing the values of the var being analyzed
+            and the variables in the comparison_group.
+        var:
+            The name of the analysis variable.
+        comparison_group:
+            The variables whose unique combination of values defines a 
+            group.
+        
+    Returns:
+        A dictionary with the chi-squared test statistic and p-value for
+        the null hypothesis that the proportion of the sample with this
+        value for var is the same across groups.
+    '''
     var_df['Is Value'] = np.select(
         [
             var_df[var].isnull(),
@@ -229,6 +355,22 @@ def compare_proportion_true(
     var:str,
     comparison_group:list[str]
 ):
+    '''Compare the distribution of true values across groups.
+    
+    Arguments:
+        var_df:
+            A dataframe containing the values of the var being analyzed
+            and the variables in the comparison_group.
+        var:
+            The name of the analysis variable.
+        comparison_group:
+            The variables whose unique combination of values defines a 
+            group.
+    Returns:
+        A dictionary with the chi-squared test statistic and p-value for
+        the null hypothesis that the proportion of the sample with this
+        value for var is the same across groups.
+        '''
     var_df['Binary As Float'] = var_df[var].astype(float)
     group_object = (
         var_df[comparison_group + ['Binary As Float']]
@@ -268,7 +410,8 @@ def get_stats(var_type:str, ser:pd.Series, var:str):
             The variable name
 
     Returns:
-        A dictionary of information which will populate var_dict['Stats']
+        A var-type -dependent dictionary of information on general stats
+        for the variable var.
     '''
     if var_type == 'Constant':
         return {'Constant Value': ser.iloc[0]}
@@ -283,6 +426,17 @@ def get_stats(var_type:str, ser:pd.Series, var:str):
 
 
 def get_categorical_stats(ser:pd.Series, var:str):
+    '''Get summary stats for a categorical variable.
+    
+    Arguments:
+        ser:
+            A series object with the sample data for the variable.
+        var:
+            The variable name.
+    
+    Returns:
+        A dictionariy with summary stats.
+    '''
     my_dict = dict()
     my_dict['Count Non Missing'] = len(ser.dropna())
     my_df = (
@@ -296,6 +450,18 @@ def get_categorical_stats(ser:pd.Series, var:str):
 
 
 def get_numerical_stats(ser:pd.Series,var:str):
+    '''Get summary statistics for a numerical variable.
+
+    Arguments:
+        ser:
+            A series object with the sample data for the variable.
+        var:
+            The variable name.
+    
+    Returns:
+        A dictionary with summary stats.
+
+    '''
     my_dict = dict()
     my_dict['Mean'] = ser.mean()
     my_dict['Standard Dev'] = ser.std()
@@ -344,6 +510,14 @@ def get_numerical_stats(ser:pd.Series,var:str):
 
 
 def get_binary_stats(ser:pd.Series):
+    '''Get summary stats for a binary variable.
+    
+    Arguments:
+        ser:
+            A series object with sample data for the variable.
+    Returns:
+        A dictionary with summary stats for the binary variable.   
+    '''
     my_dict = dict()
     my_dict['Not Null Count'] = len(ser.dropna())
     my_dict['True Count'] = ser.sum()
@@ -363,6 +537,23 @@ def compare_nulls_across_groups(
     has_nulls:bool,
     var_df:pd.DataFrame
 ):
+    '''Test if the proportion of nulls is consistent across groups.
+
+    Arguments:
+        var_df:
+            A dataframe containing the values of the var being analyzed
+            and the variables in the comparison_group.
+        var:
+            The name of the analysis variable.
+        comparison_group:
+            The variables whose unique combination of values defines a 
+            group.
+        has_nulls:
+            A bool indicating if the variable has null values.
+    Returns:
+        A dictionary with chi-squre test stat and p-value for testing
+        whether all groups have the same proportion of nulls.
+    '''
     if not has_nulls:
         return 'No Nulls'
     else:
@@ -400,6 +591,30 @@ def compare_nulls_across_groups(
 
 
 def get_variable_type(unique_array:np.ndarray, is_numerical:bool):
+    '''Get the general type (not python datatype) of a variable.
+    
+    For data cleaning and exploratory analysis purposes, it's useful to
+    know which of the following categories data falls into:
+    [
+        Constant
+        Constant With Nulls
+        Binary Bool
+        Binary Numerical
+        Binary Categorical
+        Numerical
+        Categorical
+    ]
+    
+    Arguments:
+        unique_array:
+            The list of unique values in the variable's sample.
+        is_numerical:
+            A bool which indicates whether the pandas datatype of the
+            variable is numerical.
+        
+    Returns:
+        A string with one of the above listed types
+    '''
     if len(unique_array) == 1:
         return 'Constant'
     elif (
@@ -418,8 +633,8 @@ def get_variable_type(unique_array:np.ndarray, is_numerical:bool):
             return 'Binary Categorical'
         else:
             return 'Categorical'
-    
-    
+
+
 def null_info(ser:pd.Series):
     '''Get info on the nulls of a series.'''
     if len(ser) == 0:
@@ -430,7 +645,27 @@ def null_info(ser:pd.Series):
     return null_info
 
 
-def equal_proportions_test_two_samples(n_a:int, p_hat_a:float, n_b:int, p_hat_b:float):
+def equal_proportions_test_two_samples(
+    n_a:int,
+    p_hat_a:float,
+    n_b:int,
+    p_hat_b:float
+):
+    '''Test whether two samples have equal proportions of a variable.
+    
+    Arguments:
+        n_a:
+            The number of observations in group a
+        p_hat_a:
+            The proportion of hits in group a
+        n_b:
+            The number of observations in group b
+        p_hat_b:
+            The proportion of hits in group b
+    
+    Returns
+        A dict with the z-score and p-value.
+    '''
     p_hat = (
         (n_a*p_hat_a + n_b*p_hat_b)
         /(n_a + n_b)
@@ -448,6 +683,17 @@ def equal_proportions_test_two_samples(n_a:int, p_hat_a:float, n_b:int, p_hat_b:
 
 
 def equal_proportions_test_many_samples(n_list:list, hits_list:list):
+    '''Test whether the probability of a hit is the same across samples.
+    
+    Arguments:
+        n_list:
+            A list of the number of observations in each sample.
+        hits_list:
+            A list of the number of hits in each sample.
+    
+    Returns:
+        A dictionary with the chi-squared stat and P-Value of the test
+    '''
     test = proportions_chisquare(
         count = hits_list,
         nobs = n_list
